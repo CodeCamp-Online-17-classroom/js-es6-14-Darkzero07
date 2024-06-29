407df1cf6388e0fb0c9d925d520a2d063bf1b421
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-29 11:56:16 +0700

    js lab 158

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+// ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไรและเพราะอะไร
+
+let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
+let {numPlanets, yearNeptuneDiscovered } = facts;
+console.log(numPlanets); // 8 เป็นการใช้ object destructuring เพื่อดึงค่าที่อยู่ใน object facts ของ numPlanets
+console.log(yearNeptuneDiscovered); // 1846 เป็นการใช้ object destructuring เพื่อดึงค่าที่อยู่ใน object facts ของ yearNeptuneDiscovered
